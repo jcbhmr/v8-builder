a8d3aac815771876da705411d4941e11946f0f3e
Jacob Hummer <jcbhmr@outlook.com>
2024-04-13 22:03:12 +0000

    save

--- setup-depot_tools/main.sh
+++ setup-depot_tools/main.sh
@@ -5,6 +5,7 @@ if [[ $RUNNER_DEBUG == 1 ]]; then set -x; fi
 cd ~
 git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 echo "$HOME/depot_tools" >> "$GITHUB_PATH"
+export PATH="$HOME/depot_tools:$PATH"
 echo "Installed depot_tools to $HOME/depot_tools and added to \$PATH"
 echo "See https://commondatastorage.googleapis.com/chrome-infra-docs/flat/depot_tools/docs/html/depot_tools.html"
 
